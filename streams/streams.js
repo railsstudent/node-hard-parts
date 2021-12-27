43c860fbadd573a234b560e000d9d4c303fe007f
Connie Leung <cleung334@gmail.com>
2021-12-27 22:15:24 +0800

    feat: finished streams exercise
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- streams/streams.js
+++ streams/streams.js
@@ -1,15 +1,37 @@
 const fs = require('fs');
-const through = require('through2');
+const through2 = require('through2');
 
-/* Create a read stream here
-const readPoemStream = ???
-*/
+const { Transform } = require('readable-stream')
+ 
+const readPoemStream = fs.createReadStream('./on-joy-and-sorrow-emoji.txt')
 
-/* Create a write stream here
-const writePoemStream =
-*/
+// readPoemStream.on('data', (data) => {
+//     const strData = data.toString()
+//     const transformedData = strData.replace(/:\)/gi, 'joy').replace(/:\(/gi, 'sorrow')
+//     console.log(transformedData)
+// })
 
-/* EXTENSION: Create a transform stream (modify the read stream before piping to write stream)
-const transformStream = ???
+/* Create a write stream here */
+const writePoemStream = fs.createWriteStream('./on-joy-and-sorrow-emoji-2.txt')
+
+
+/* EXTENSION: Create a transform stream (modify the read stream before piping to write stream) */
+const transformStream = through2(function(chunk, encoding, callback) {
+        const strChunk = chunk.toString()
+        const transformedData = strChunk.replace(/:\)/gi, 'joy').replace(/:\(/gi, 'sorrow')
+        this.push(transformedData)
+        callback()
+    })
 readPoemStream.pipe(transformStream).pipe(writePoemStream)
-*/
+
+
+const transformStream2 = new Transform({
+    transform(chunk, encoding, callback) {
+        const strChunk = chunk.toString()
+        const transformedData = strChunk.replace(/:\)/gi, 'joy2').replace(/:\(/gi, 'sorrow2')
+        this.push(transformedData)
+        callback()
+    }
+})
+const writePoemStream2 = fs.createWriteStream('./on-joy-and-sorrow-emoji-3.txt')
+readPoemStream.pipe(transformStream2).pipe(writePoemStream2)
